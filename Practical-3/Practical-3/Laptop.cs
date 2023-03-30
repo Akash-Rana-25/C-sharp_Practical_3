94f3c2c604fe7602df8fb47ba63ad268045320e2
Akash-Rana-25 <akashrana.work.gmail.com>
2023-03-30 11:52:47 +0530

    OOP concept Practical-3 added to feature branch

---
+++ Practical-3/Practical-3/Laptop.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_3
+{
+
+    // Abstraction 
+    class Laptop
+    {
+        private string brand;
+        private string model;
+
+        public string Brand
+        {
+            get
+            {
+                return brand;
+            }
+            set
+            {
+                brand = value;
+            }
+        }
+        public string Modal
+        {
+            get
+            {
+                return model;
+            }
+            set
+            {
+                model = value;
+            }
+        }
+
+        public void LaptopDetails()
+        {
+            Console.WriteLine("Laptop brand is" + brand);
+            Console.WriteLine("Laptop modal is" + model);
+        }
+        private void MotherBoradinfo()
+        {
+            Console.WriteLine("MotherBorad Information ");
+        }
+
+
+    }
+}
---
+++ Practical-3/Practical-3/Sponsor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_3
+{
+
+    // Inheritance
+    class Sponsor
+    {
+        protected string owner = "Rakuteen";
+    }
+    class Team : Sponsor
+    {
+        private string teamName = "Dot Net";
+
+        /// <summary>
+        /// This method concate  two Strings and display Full string
+        /// </summary>
+        public void Printinfo()
+        {
+            string fullString;
+            fullString = owner + " " + teamName;
+            Console.WriteLine(fullString);
+            //return fullString;
+        }
+    }
+}
